2e77b3d6a9fba7a81ae322f79b6cd62ec1e832bc
denis88k <denis88.k@mail.ru>
2023-06-22 00:08:03 +0300

    ref: function btnShowMore

--- src/js/_components.js
+++ src/js/_components.js
@@ -12,37 +12,40 @@ modal('.promo__header-modal-bestPrice', '.modal-bestPrice', '.modal-bestPrice .m
 import validateForms from './components/validation.js';
 
 document.querySelectorAll('.form').forEach(form => {
-  validateForms(form);
-  form.querySelectorAll('.form__input').forEach(input => {
-    input.onblur = function (e) {
-      if (this.classList.contains('input__tel')) {
-        // console.log(e.target.inputmask.unmaskedvalue().length, 'blur tel');
-        const targetLength = e.target.inputmask.unmaskedvalue().length;
-        if (0 < targetLength && targetLength < 10) {
-          this.classList.add('just-validate-error-field');
-        } else if (0 < targetLength) {
-          this.classList.add('just-validate-success-field');
-        }
-      }
-      if (this.classList.contains('input__name')) {
-        // console.log(e.target.value.length, 'blur name');
-        const targetLength = e.target.value.length;
-        if (0 < targetLength && targetLength < 3) {
-          this.classList.add('just-validate-error-field');
-        } else if (0 < targetLength) {
-          this.classList.add('just-validate-success-field');
-        }
-      }
-    };
-    input.onfocus = function () {
-      // if (this.classList.contains('input__tel')) {
-      //   console.log(this.inputmask.unmaskedvalue(), 'focus tel');
-      // }
-      this.classList.contains('just-validate-error-field') && this.classList.remove('just-validate-error-field');
-      this.classList.contains('just-validate-success-field') && this.classList.remove('just-validate-success-field');
-      // if (this.classList.contains('input__name')) {
-      //   console.log(this, 'focus name');
-      // }
-    };
-  });
+	validateForms(form);
+	form.querySelectorAll('.form__input').forEach(input => {
+		input.onblur = function (e) {
+			if (this.classList.contains('input__tel')) {
+				// console.log(e.target.inputmask.unmaskedvalue().length, 'blur tel');
+				const targetLength = e.target.inputmask.unmaskedvalue().length;
+				if (0 < targetLength && targetLength < 10) {
+					this.classList.add('just-validate-error-field');
+				} else if (0 < targetLength) {
+					this.classList.add('just-validate-success-field');
+				}
+			}
+			if (this.classList.contains('input__name')) {
+				// console.log(e.target.value.length, 'blur name');
+				const targetLength = e.target.value.length;
+				if (0 < targetLength && targetLength < 3) {
+					this.classList.add('just-validate-error-field');
+				} else if (0 < targetLength) {
+					this.classList.add('just-validate-success-field');
+				}
+			}
+		};
+		input.onfocus = function () {
+			// if (this.classList.contains('input__tel')) {
+			//   console.log(this.inputmask.unmaskedvalue(), 'focus tel');
+			// }
+			this.classList.contains('just-validate-error-field') && this.classList.remove('just-validate-error-field');
+			this.classList.contains('just-validate-success-field') && this.classList.remove('just-validate-success-field');
+			// if (this.classList.contains('input__name')) {
+			//   console.log(this, 'focus name');
+			// }
+		};
+	});
 });
+
+import init from './components/yMap.js';
+ymaps.ready(init);
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -57,14 +57,18 @@ const removeClassArray = (elements, cls) => {
 		element?.classList.remove(cls);
 	});
 };
+const closestElement = (element, cls) => {
+	document.querySelectorAll(element)[0].closest(cls);
+};
+// console.log(closestElement('.years__checkbox-block', 'isVisible'));
 
-// функция компонент для проставления галочек на checkboxBlock's
+// функция компонент для проставления галочек на checkboxBlock'ы
 const checkboxComponent = block => {
 	const componentCheckbox = document.querySelector(`.${block}__checkbox`);
 	componentCheckbox.addEventListener('click', e => {
 		const componentCheckboxBlock = e.target.closest(`.${block}__checkbox-block`);
 		if (!componentCheckboxBlock) {
-			console.log('вне componentCheckboxBlock');
+			// console.log('вне componentCheckboxBlock');
 			return;
 		}
 		toggleClass(componentCheckboxBlock, 'active');
@@ -75,46 +79,171 @@ const checkboxComponent = block => {
 // blocksShow - блоки, которые нужно отобразить
 // btn - кнопка
 // btnText - элемент где нужно отобразить текст: показать, скрыть
-// countBlockVisible - кол-во, которое отобразиться в первый раз
+// countBlock - кол-во, которое отобразиться в первый раз 6/3
 // textHidden - текст, когда спрятаны часть блоков
 // textOpen - текст, когда отображены все блоки
-const btnShowMore = (blocksShow, btn, btnText = '', countBlockVisible, textHidden, textOpen) => {
-	// checkboxBlocks->blocks
+
+// TODO: функция проверки кол-ва отображаемых блоков
+// изначально удалить со всех блоков класс isVisible
+// если кол-во блоков больше, чем требуется, то:
+// 		1) отобразить требуемое кол-во блоков
+// 		-2) кнопке btnShowMore сделать display: block;
+// если кол-во блоков меньше, чем требуется, то:
+// 		1) отобразить все элементы
+// 		-2) кнопка btnShowMore сделать display: none;
+
+// const checkLengthVisible = (blocksShow, countBlockVisible, btn) => {
+// 	const blocks = document.querySelectorAll(blocksShow);
+// 	removeClassArray(blocks, 'isVisible');
+
+// 	if (blocks.length > countBlockVisible) {
+// 		for (let i = 0; i < countBlockVisible; i++) {
+// 			addClass(blocks[i], 'isVisible');
+// 		}
+// 	}
+// 	if (blocks.length <= countBlockVisible) {
+// 		blocks.forEach(block => addClass(block, 'isVisible'));
+// 		btn.style.display = 'none';
+// 	}
+// };
+
+// TODO: функция кнопки showMore
+// переменная сделать глобальной isShowMore=false --- свернуто / отображены все
+// удалить прослушку по кнопке
+// если блоков больше, чем требуется, то:
+// 		1) кнопке добавить надпись "показать все"
+// 		2) display: block
+// 		3) кнопке добавить прослушку
+// если блоков меньше, чем требуется, то:
+// 		1) display: none
+// let isShowMoreYears = false;
+// const btnShowMore = (blocksShow, countBlockVisible, btn, btnText = '', textHidden, textOpen) => {
+// 	const blocks = document.querySelectorAll(blocksShow);
+// 	const btnShowMore = document.querySelector(btn);
+// 	btnShowMore.removeEventListener('click', btnShowMoreClick);
+// 	if (blocks.length > countBlockVisible) {
+// 		btnShowMore.style.display = 'block';
+// 		let btnShowMoreText;
+// 		btnText ? (btnShowMoreText = btnShowMore.querySelector(btnText)) : (btnShowMoreText = btnShowMore);
+// 		btnShowMore.addEventListener('click', btnShowMoreClick);
+// 	}
+// 	if (blocks.length <= countBlockVisible) {
+// 		btnShowMore.style.display = 'none';
+// 	}
+// };
+
+// TODO: функция клика по кнопке:
+// 		1) переменная isShowMore=!isShowMore
+// 		2) показывает доп элементы или скрывает их
+// 		3) меняет надпись в кнопке на "свернуть" или "показать все"
+// const btnShowMoreClick = () => {
+// 	for (let i = countBlockVisible; i <= blocks.length; i++) {
+// 		isShowMore ? removeClass(blocks[i], 'isVisible') : addClass(blocks[i], 'isVisible');
+// 	}
+// 	isShowMore ? removeClass(btnShowMore, 'active') : addClass(btnShowMore, 'active');
+// 	btnShowMoreText.innerHTML = isShowMore ? textOpen : textHidden;
+// 	isShowMore = !isShowMore;
+// };
+// функция выполняемая при клике
+// const btnShowMoreClick = () => {
+// 	console.log(isShowMore);
+// 	for (let i = countBlockVisible; i <= blocks.length; i++) {
+// 		isShowMore ? removeClass(blocks[i], 'isVisible') : addClass(blocks[i], 'isVisible');
+// 	}
+// 	toggleClass(btnShowMore, 'active');
+// 	// isShowMore ? removeClass(btnShowMore, 'active') : addClass(btnShowMore, 'active');
+// 	btnShowMoreText.innerHTML = isShowMore ? textOpen : textHidden;
+// 	isShowMore = !isShowMore;
+// };
+// const blockVisibleAndBtnShowMore = (blocksShow, countBlockVisible, btn, btnText = '') => {
+const blockVisibleAndBtnShowMore = (...params) => {
+	// checkLengthVisible(blocksShow, countBlockVisible, btn);
+	// btnShowMore(blocksShow, countBlockVisible, btn, btnText='',textHidden, textOpen);
+
+	const [blocksShow, countBlockVisible, btn, btnText = '', textOpen] = params;
+	const textHidden = 'Свернуть';
 	const blocks = document.querySelectorAll(blocksShow);
 	const btnShowMore = document.querySelector(btn);
-	let btnShowMoreText;
-	let isShowMore = false;
-	let countBlock = countBlockVisible; // сколько в начале отобразить шт
+	removeClassArray(blocks, 'isVisible');
 
-	btnText ? (btnShowMoreText = btnShowMore.querySelector(btnText)) : (btnShowMoreText = btnShowMore);
+	if (blocks.length > countBlockVisible) {
+		let isShowMore = false;
 
-	const toggleClassActive = isShowMore => {
-		for (let i = countBlock + 1; i < blocks.length; i++) {
-			isShowMore ? removeClass(blocks[i], 'isVisible') : addClass(blocks[i], 'isVisible');
-		}
-		isShowMore ? removeClass(btnShowMore, 'active') : addClass(btnShowMore, 'active');
-	};
-
-	const toggleBtnText = isShowMore => {
-		btnShowMoreText.innerHTML = isShowMore ? textOpen : textHidden;
-	};
-
-	if (blocks.length > countBlock) {
-		for (let i = 0; i <= countBlock; i++) {
+		for (let i = 0; i < countBlockVisible; i++) {
 			addClass(blocks[i], 'isVisible');
 		}
-		btnShowMore.addEventListener('click', () => {
-			toggleClassActive(isShowMore);
-			toggleBtnText(isShowMore);
-			isShowMore = !isShowMore;
-		});
-	}
 
+		btnShowMore.style.display = 'flex';
+		let btnShowMoreText;
+		btnText ? (btnShowMoreText = btnShowMore.querySelector(btnText)) : (btnShowMoreText = btnShowMore);
+		btnShowMoreText.innerText = textOpen;
+
+		// btnShowMore.addEventListener('click', btnShowMoreClick.bind(null, countBlockVisible, blocks, btnShowMore, btnShowMoreText, textOpen, textHidden));
+		const btnShowMoreClick = () => {
+			console.log(isShowMore);
+			for (let i = countBlockVisible; i <= blocks.length; i++) {
+				isShowMore ? removeClass(blocks[i], 'isVisible') : addClass(blocks[i], 'isVisible');
+			}
+			toggleClass(btnShowMore, 'active');
+			// isShowMore ? removeClass(btnShowMore, 'active') : addClass(btnShowMore, 'active');
+			btnShowMoreText.innerText = isShowMore ? textOpen : textHidden;
+			isShowMore = !isShowMore;
+		};
+		btnShowMore.removeEventListener('click', btnShowMoreClick);
+		btnShowMore.addEventListener('click', btnShowMoreClick);
+	}
 	if (blocks.length <= countBlockVisible) {
 		blocks.forEach(block => addClass(block, 'isVisible'));
-		btnShowMore.style.display = 'none';
+		btn.style.display = 'none';
 	}
 };
+
+// =======проверка кол-ва первых видимых блоков=======
+
+// ====обнуление кнопки "ПОКАЗАТЬ ЕЩЁ" и проверка длины первых видимых блоков=====
+// const resetBtnShowMore = (btn, blocksShow, countBlockVisible) => {
+// 	const btnClick = document.querySelector(btn);
+// 	containsClass(btnClick, 'active') && btnClick.click();
+// 	checkLengthFirstVisible(blocksShow, countBlockVisible, btnClick);
+// };
+
+// const btnShowMore = (blocksShow, btn, btnText = '', countBlockVisible, textHidden, textOpen) => {
+// 	// checkboxBlocks->blocks
+// 	const blocks = document.querySelectorAll(blocksShow);
+// 	const btnShowMore = document.querySelector(btn);
+// 	let btnShowMoreText;
+// 	let isShowMore = false;
+// 	let countBlock = countBlockVisible; // сколько в начале отобразить шт
+
+// 	btnText ? (btnShowMoreText = btnShowMore.querySelector(btnText)) : (btnShowMoreText = btnShowMore);
+
+// 	const toggleClassActive = isShowMore => {
+// 		for (let i = countBlock + 1; i < blocks.length; i++) {
+// 			isShowMore ? removeClass(blocks[i], 'isVisible') : addClass(blocks[i], 'isVisible');
+// 		}
+// 		isShowMore ? removeClass(btnShowMore, 'active') : addClass(btnShowMore, 'active');
+// 	};
+
+// 	const toggleBtnText = isShowMore => {
+// 		btnShowMoreText.innerHTML = isShowMore ? textOpen : textHidden;
+// 	};
+
+// 	if (blocks.length > countBlock) {
+// 		for (let i = 0; i <= countBlock; i++) {
+// 			addClass(blocks[i], 'isVisible');
+// 		}
+// 		btnShowMore.addEventListener('click', () => {
+// 			toggleClassActive(isShowMore);
+// 			toggleBtnText(isShowMore);
+// 			isShowMore = !isShowMore;
+// 		});
+// 	}
+// 	// checkLengthFirstVisible(blocksShow, countBlockVisible, btn);
+// 	if (blocks.length <= countBlockVisible) {
+// 		blocks.forEach(block => addClass(block, 'isVisible'));
+// 		btnShowMore.style.display = 'none';
+// 	}
+// };
 
 // фиксация шапки консультанта
 const consultSticky = document.querySelector('.consultant_sticky');
@@ -277,10 +406,11 @@ const resetYearsShowSelect = () => {
 checkboxComponent('years');
 
 // ====КНОПКА "ПОКАЗАТЬ ВСЕ ПОКОЛЕНИЯ"=====
-btnShowMore('.years__checkbox-block', '.years__show-more', '.years__show-more-text', 5, 'Свернуть', 'Показать все поколения');
+// btnShowMore('.years__checkbox-block', '.years__show-more', '.years__show-more-text', 5, 'Свернуть', 'Показать все поколения');
+blockVisibleAndBtnShowMore('.years__checkbox-block', 6, '.years__show-more', '.years__show-more-text', 'Показать все поколения');
+
 // ======обнуление кнопки "ПОКАЗАТЬ ВСЕ ПОКОЛЕНИЯ"======
-// TODO: кнопка показать ещё
-const resetBtnShowMoreYears = () => {};
+// resetBtnShowMore('.years__show-more', '.years__checkbox-block', 5);
 
 // ===========ПРОБЕГ===========
 // ======CHECKBOX ПРОБЕГА======
@@ -384,8 +514,8 @@ mileageInput();
 
 // ======обнуление инпута пробега======
 const resetMileageInput = () => {
-	document.querySelector('.input-from').value = 0;
-	document.querySelector('.input-to').value = 0;
+	document.querySelector('.input-from').value = '';
+	document.querySelector('.input-to').value = '';
 	removeClassArray(document.querySelectorAll('.mileage__checkbox-block'), 'active');
 };
 
@@ -404,12 +534,13 @@ const resetMileageInput = () => {
 // };
 // ownerCheckbox();
 checkboxComponent('owner');
+const resetCheckboxOwner = () => {
+	removeClassArray(document.querySelectorAll('.owner__checkbox-block'), 'active');
+};
 
 // ===========кнопка "сведения все сведения" в секции report__info===========
-btnShowMore('.report__info-block', '.report__info-btn', '', 2, 'Свернуть', 'Показать все сведения');
-const resetBtnShowMoreReport = () => {
-	document.querySelector('.report__info-btn').click();
-};
+// btnShowMore('.report__info-block', '.report__info-btn', '', 2, 'Свернуть', 'Показать все сведения');
+blockVisibleAndBtnShowMore('.report__info-block', 3, '.report__info-btn', '', 'Показать все сведения');
 
 // прокрутка до определённого сообщения
 // https://learn.javascript.ru/size-and-scroll-window
